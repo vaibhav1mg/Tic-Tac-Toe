aca84f8e75a493a551a4c51b6fcb19bb8e23624d
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-04-21 23:33:02 +0530

    need changed in css

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('tshirtButton').addEventListener('click', function() {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdDqGefF4Yf50zzwQ97ES6t-VRyJJNpF2B2yc51NI8Fh_1SQA/viewform";
+});
